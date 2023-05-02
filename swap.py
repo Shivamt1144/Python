ea8f754fb830ea3f3c4212a02302167b0abd8076
Shivam <shivamtripathi1144@gmail.com>
2023-05-02 21:45:51 +0530

    update01

---
+++ swap.py
@@ -0,0 +1,6 @@
+a=input("Enter first number ")
+b=input("Enter second number ")
+c=a
+a=b
+b=c
+print("After swapping "+ str(a) +" " +str(b))
---
+++ takinginput.py
@@ -0,0 +1,5 @@
+# input()
+# by default input function returns a string
+# input("Say my name!")
+
+print("Hello"+" "+ input("Say my name!")+" How are you?")
