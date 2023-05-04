9b7d59dd1a50359d8243a2da8d9d12194ed96e18
Shivam Tripathi <shivamtripathi1144@gmail.com>
2023-05-04 18:40:33 +0530

    new

---
+++ Basics/18exercise.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age"))
+print(f"Your current age is {age}")
+
+year=90-age
+days=(90*365)-(365*age)
+weeks=(52*90)-(52*age)
+
+print(f"You have {year} years, {weeks} weeks and {days} days to complete your 90 years")
+
