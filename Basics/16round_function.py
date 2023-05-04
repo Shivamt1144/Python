53775e98d865f1e0dca1803a50492d6532abdeae
Shivam Tripathi <shivamtripathi1144@gmail.com>
2023-05-04 18:40:48 +0530

    new

--- Basics/16round_function.py
+++ Basics/16round_function.py
@@ -1,3 +1,8 @@
 # syntax: round(number, no. of digits)
-print(round(23.22)) #this will give 23
+print(round(23.22)) #this will give 23, no. of digits is optional
 print(round(25.1238884,3)) #this will give 25.124
+print(round(7,5))
+print(round(647,-1)) #this will return the closests multiple of 10 i.e. 650, his works on power type i.e 10^(-no.of digits), in this case 10^(-(-1))=10^1=10
+print(round(643,-1)) #this will return the closests multiple of 10 i.e. 640
+print(round(674,-1000)) #this will return the closests multiple of 1000 i.e. 0
+print(round(674.1024,-1))#this will return the closests multiple of 10 i.e. 670
---
+++ Basics/17f_strings_function.py
@@ -0,0 +1,10 @@
+name="Shylock"
+age=55
+height=1.6
+
+# print("My name is "+name+" My age is "+str(age)+" My height is "+ str(height)+"m")
+# we can also print like 
+# print("My name is ",name," My age is ",str(age)," My height is ", str(height),"m")
+
+# one of the better approach is 
+print(f"My name is{name}, I am {age} years old, and my height is {height}")
